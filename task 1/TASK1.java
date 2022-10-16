53f32944695e911333f6ca3d0640a85078001396
murtazajessar <115347138+murtazajessar@users.noreply.github.com>
2022-10-16 11:54:00 +0500

    Add files via upload

---
+++ task 1/TASK1.java
@@ -0,0 +1,27 @@
+
+
+public class TASK1 
+{
+    public static void main (String[] args)
+    {
+        
+
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello"; 
+        double myDouble = 0.0; // double
+        short myShort = 0; // short
+
+        System.out.println("Integer " + myNum);
+        System.out.println("float " + myFloatNum);
+        System.out.println("char " + myLetter);
+        System.out.println("boolean " + myBool);
+        System.out.println("double " + myDouble);
+        System.out.println("short " + myShort);
+        System.out.println("String " + myText);
+
+
+    }
+}
